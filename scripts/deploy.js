1b5fe0d8fc8650af6562e6d09cffe9561078002a
andre.kortkamp <andre.kortkamp@genezys.io>
2023-08-08 10:22:29 -0300

    update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,33 +1,60 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = hre.ethers.parseEther("0.001");
-
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+async function getBalance(address) {
+  const balanceBigInt = await hre.ethers.provider.getBalance(address);
+  return hre.ethers.utils.formatEther(balanceBigInt);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+async function printBalances(addresses) {
+  let idx = 0;
+  for (const address of addresses) {
+    console.log(`Address ${idx} balance: `, await getBalance(address));
+    idx ++;
+  }
+}
+
+async function printPost(posts) {
+  for (const post of posts) {
+    const timestamp = post.timestamp;
+    const tipper = post.name;
+    const tipperAddress = post.from;
+    const message = post.message;
+    console.log(`At ${timestamp}, ${tipper} (${tipperAddress}) said: "${message}"`);
+  }
+}
+
+async function main() {
+  const [owner, tipper] = await hre.ethers.getSigners();
+
+  const DonateWeb3 = await hre.ethers.getContractFactory("DonateWeb3");
+  const donateWeb3 = await DonateWeb3.deploy();
+
+  await donateWeb3.deployed();
+  console.log("DonateWeb3 deployed to:", donateWeb3.address);
+
+  const addresses = [owner.address, tipper.address, donateWeb3.address];
+  console.log("start");
+  await printBalances(addresses);
+
+  const tip = {value: hre.ethers.utils.parseEther("1")};
+  await donateWeb3.connect(tipper).donate("Andre", "Hello World", tip);
+
+  console.log("donate count");
+  await printBalances(addresses);
+
+  await donateWeb3.connect(owner).withdrawTips();
+
+  console.log("withdrawTips");
+  await printBalances(addresses);
+
+  console.log("posts");
+  const posts = await donateWeb3.getPost();
+  printPost(posts);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
